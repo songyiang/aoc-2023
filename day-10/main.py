51c5f814bbae86c1b3e417a79e9134a9a5d5533c
songyiang <ang.songyi1998@gmail.com>
2023-12-10 22:24:28 +0800

    Attempt Day 10 Part 2 using ray casting algorithm

--- day-10/main.py
+++ day-10/main.py
@@ -75,3 +75,78 @@ while isAtStart or len(curr) != 1:
 print(numSteps)
 
 ### PART 2 ###
+
+def getNextPosMovement(pos):
+    directions = []
+    curr = maze[pos[0]][pos[1]]
+    if curr == '|':
+        directions = [UP, DOWN]
+    elif curr == '-':
+        directions = [RIGHT, LEFT]
+    elif curr == 'L':
+        directions = [UP, RIGHT]
+    elif curr == 'J':
+        directions = [UP, LEFT]
+    elif curr == '7':
+        directions = [DOWN, LEFT]
+    elif curr == 'F':
+        directions = [RIGHT, DOWN]
+    elif curr == 'S':
+        directions = [UP, RIGHT, DOWN, LEFT]
+
+    for direction in directions:
+        r, c = addPairs(pos, direction)
+        if r >= 0 and r < numRows and c >= 0 and c < numCols and canMove(maze[r][c], direction):
+            if (r, c) not in traced: # prevent dfs from non-termination
+                return (r, c), direction
+
+def getDirectionStr(direction):
+    if direction == UP:
+        return 'U'
+    elif direction == RIGHT:
+        return 'R'
+    elif direction == DOWN:
+        return 'D'
+    elif direction == LEFT:
+        return 'L'
+
+# DFS
+curr = start
+traced = set()
+traced.add(start)
+directionalMaze = [[' ' for _ in range(numCols)] for _ in range(numRows)]
+while True:
+    next = getNextPosMovement(curr)
+    if next is None: # reached starting position
+        break
+    nextPos, direction = next
+    traced.add(nextPos)
+    directionalMaze[curr[0]][curr[1]] = getDirectionStr(direction)
+    curr = nextPos
+
+# Ray casting algorithm
+insidePoints = set()
+for r in range(numRows):
+    isInside = False
+    prev = None
+    consecutivePoints = []
+    for c in range(numCols):
+        if (r, c) in traced:
+            consecutivePoints = []
+            curr = directionalMaze[r][c]
+            if curr == 'U' or curr == 'D':
+                isInside = prev == None or prev == curr
+                prev = curr
+        elif isInside:
+            insidePoints.add((r, c))
+            consecutivePoints.append((r, c))
+    if isInside:
+        for point in consecutivePoints:
+            insidePoints.remove(point)
+print(len(insidePoints))
+
+# Visualise directional maze
+for point in insidePoints:
+    directionalMaze[point[0]][point[1]] = 'X'
+for row in directionalMaze:
+    print(row)
